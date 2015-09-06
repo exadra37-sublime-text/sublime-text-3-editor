c813c80b5981722a7efe352661e477cee999cb8f
Exadra37 <exadra37@gmail.com>
2015-09-06 23:31:23 +0100

    Add source file to define colours to use in terminal output.

---
+++ src/Source/bash-colours.source.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# @author Exadra37(Paulo Silva) <exadra37ingmailpointcom>
+# @since  2014/11/11
+# @link   http://exadra37.com
+
+# defining colors vars
+bc_black=`tput setaf 0`;
+
+bc_red=`tput setaf 1`;
+
+bc_green=`tput setaf 2`;
+
+bc_yellow=`tput setaf 3`;
+
+bc_blue=`tput setaf 4`;
+
+bc_magenta=`tput setaf 5`;
+
+bc_cyan=`tput setaf 6`;
+
+bc_white=`tput setaf 7`;
+
+bc_reset=`tput sgr0`;
